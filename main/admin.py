b6a0cef0f77107cd125e6eed306a4c798013e645
Vlad618 <vladlopyp@gmail.com>
2026-05-14 19:58:42 +0300

    Lab 4: Django models with mods system

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Game, Mod, ModAuthor
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name", "genre", "created_at", "updated_at")
+
+
+@admin.register(ModAuthor)
+class ModAuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "experience", "created_at", "updated_at")
+
+
+@admin.register(Mod)
+class ModAdmin(admin.ModelAdmin):
+    list_display = ("name", "game", "author", "created_at", "updated_at")
--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ModAuthor(models.Model):
+    name = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Mod(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    # 🔗 зв’язок між таблицями (ОБОВ’ЯЗКОВО)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    author = models.ForeignKey(ModAuthor, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
